18348e2ae2558011e1354364d8d82de0f3d349f5
W. Jones <waltonsj3@gmail.com>
2023-06-09 17:30:03 -0400

    begining basic setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/global.css';
+import '../styles/main.css';
 
 import type { AppProps } from 'next/app';
 
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,19 +2,38 @@ import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
 
 const About = () => (
-  <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
+  <Main meta={<Meta title="About Ether Oasis" description="Lorem ipsum" />}>
     <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
-      recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
-      voluptatibus distinctio recusandae autem esse explicabo molestias officia
-      placeat, accusamus aut saepe.
+      Welcome to Ether Oasis, the web app that aims to provide users with a
+      seamless way to view and manage their Ethereum wallets as a personalized
+      profile on the Ethereum blockchain. Ether Oasis endeavors to simplify the
+      complexities of cryptocurrencies, making it accessible to users of all
+      levels of expertise.
     </p>
     <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
-      recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
-      voluptatibus distinctio recusandae autem esse explicabo molestias officia
-      placeat, accusamus aut saepe.
+      Ether Oasis offers a user-friendly interface that centralizes your
+      Ethereum assets, including tokens and non-fungible tokens (NFTs). Through
+      our app, users can effortlessly track their token balances, transaction
+      history, and market values. Our primary focus is to present this
+      information in a clear and intuitive manner, allowing you to easily
+      navigate and understand your Ethereum profile.
     </p>
+    <p>
+      As Ether Oasis continues to evolve, our ambitions expand alongside it. We
+      envision enhancing the app with advanced features and analytics tools that
+      provide deeper insights into your on-chain activities. Our goal is to
+      empower you to make informed decisions by offering valuable data and
+      visualizations related to your Ethereum profile.
+    </p>
+    <p>
+      Join Ether Oasis on this exciting journey as we strive to build a valuable
+      resource for individuals looking to explore and leverage the potential of
+      Ethereum. We are committed to listening to user feedback and incorporating
+      the latest industry trends to ensure that Ether Oasis remains a trusted
+      and comprehensive platform for managing and exploring your Ethereum
+      profile.
+    </p>
+
   </Main>
 );
 
