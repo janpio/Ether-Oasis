2e30a4a941ae319ed7af6eac85cb72785519e641
W. Jones <waltonsj3@gmail.com>
2023-07-20 10:50:09 -0400

    feat: adds script to seed db with contract information

---
+++ scripts/seedContracts.mjs
@@ -0,0 +1,48 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-await-in-loop */
+/* eslint-disable guard-for-in */
+import fs from 'fs';
+import path from 'path';
+
+import { PrismaClient } from '@prisma/client';
+
+let prisma;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  const globalWithPrisma = global;
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient();
+  }
+  prisma = globalWithPrisma.prisma;
+}
+
+async function seedDatabase() {
+  const filePath = path.resolve('./updatedContracts.json');
+  const rawData = fs.readFileSync(filePath, 'utf-8');
+  const contracts = JSON.parse(rawData);
+
+  for (const address in contracts) {
+    const token = contracts[address];
+    if (token) {
+      const { name, type, abi } = token;
+      await prisma.contract.create({
+        data: {
+          address, // Using the unique address as the identifier
+          name,
+          type,
+          abi: JSON.stringify(abi), // Store ABI as a JSON string in the database
+        },
+      });
+    }
+  }
+}
+
+seedDatabase()
+  .catch((error) => {
+    console.error(error);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
